3bc8d37557943644503498623746f40e4e75bf3c
MrBenz <dkostya05@gmail.com>
2019-12-04 11:59:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,24 +10,7 @@ Config.set('graphics', 'width', 400)
 Config.set('graphics', 'height', 700)
 
 
-class MyApp(Screen):
-
-    def build(self):
-        bl = BoxLayout(orientation='vertical', padding=15)
-        gl = GridLayout(cols=2, spacing=15, size_hint=(1, .4))
-        lbl = Label(text='some_text')
-        bl.add_widget(lbl)
-
-        gl.add_widget(Button(text="ANS_0"))
-        gl.add_widget(Button(text="ANS_1"))
-        gl.add_widget(Button(text="ANS_2"))
-        gl.add_widget(Button(text="ANS_3"))
-
-        bl.add_widget(gl)
-        return bl
-
-
-class MenuScreen(App):
+class MenuScreen(Screen):
 
     def build(self):
         bl = BoxLayout(orientation='vertical', padding=15)
@@ -39,6 +22,21 @@ class MenuScreen(App):
 
 sm = ScreenManager()
 sm.add_widget(MenuScreen(name='menu'))
+
+class MyApp(App):
+
+    def build(self):
+        bl = BoxLayout(orientation='vertical', padding=15)
+        gl = GridLayout(cols=2, spacing=15, size_hint=(1, .4))
+        lbl = Label(text='some_text')
+        bl.add_widget(lbl)
+
+        gl.add_widget(Button(text="ANS_0"))
+        gl.add_widget(Button(text="ANS_1"))
+        gl.add_widget(Button(text="ANS_2"))
+        gl.add_widget(Button(text="ANS_3"))
+        bl.add_widget(gl)
+        return bl
 
 if __name__ == '__main__':
     MyApp().run()
