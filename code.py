28fdab638490e47988acb9ef54d2b921f277e2b9
MrBenz <dkostya05@gmail.com>
2019-12-09 17:39:35 +0300

    Update code.py

--- code.py
+++ code.py
@@ -3,5 +3,3 @@ def key(slovar, number):
     for i in slovar:
         a.append(i)
     return a[number-1]
-Capitals = {'Russia': 'Moscow', 'Ukraine': 'Kiev', 'USA': 'Washington'}
-print(key(Capitals ,2))
