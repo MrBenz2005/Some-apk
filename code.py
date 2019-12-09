f37901796bdfce81376edc3c78ac6babb45297ee
MrBenz <dkostya05@gmail.com>
2019-12-09 17:00:56 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,15 +1,2 @@
-from kivy.app import App
-from kivy.uix.widget import Widget
+def slovars():
 
-
-class PongGame( Widget):
-    pass
-
-
-class PongApp( App):
-    def build( self):
-        return PongGame()
-
-
-if __name__ ==  '__main__':
-    PongApp().run()
