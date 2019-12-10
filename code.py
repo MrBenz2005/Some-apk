2acdb792d2da1f9f002a8d250517d148b8a967eb
ropraname <45720319+ropraname@users.noreply.github.com>
2019-12-10 14:31:45 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,5 +1,13 @@
+math = {'qst1': ['ask', 'ask1', 'ask2', 'ask3'],
+        'qst2': ['asks', 'asks1', 'asks2', 'asks3']}
+
+idk = {'qst1': ['ask', 'ask1', 'ask2', 'ask3'],
+       'qst2': ['asks', 'asks1', 'asks2', 'asks3']}
+
+
 def key(slovar, number):
-    a = []
-    for i in slovar:
-        a.append(i)
-    return a[number-1]
+    keys = []
+    if number <= len(slovar):
+        for i in slovar:
+            keys.append(i)
+    return keys[number - 1]
